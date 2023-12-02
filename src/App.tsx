1402b4bab11f3a241208fb7e2a1789b1e18cebd4
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-02 16:48:20 -0300

    chore: add initial project files and setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "@/styles/globals.css";
+import { Button } from "@/components/shadcn/ui";
+
+const App: React.FC = () => {
+  return <Button className="rounded-full text-lg">Button</Button>;
+};
+
+export default App;
