f1c8975f248344e7a4997bf19e2bbf7623ea0065
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-04 13:19:16 -0300

    refactor: LaunchResults component and add SuccessAndFailureLaunchesCount component

--- src/pages/Home/components/LaunchResults.tsx
+++ src/pages/Home/components/LaunchResults.tsx
@@ -1,21 +1,11 @@
 import { Card } from "@/components/shadcn/ui";
-import useLaunchesStats from "@/hooks/useLaunchStats";
+import PieChart from "./PieChart";
 import React from "react";
-import { FaCheck, FaTimes } from "react-icons/fa";
+import { useWindowSize } from "usehooks-ts";
+import SuccessAndFailureLaunchesCount from "./SuccessAndFailureLaunchesCount";
 
 const LaunchResults: React.FC = () => {
-  const { isLoading, launchesStats } = useLaunchesStats();
-
-  if (isLoading) return <p>Carregando...</p>;
-
-  const successCount = launchesStats?.reduce(
-    (acc, curr) => acc + curr.successful,
-    0,
-  );
-  const failureCount = launchesStats?.reduce(
-    (acc, curr) => acc + curr.failed,
-    0,
-  );
+  const { width } = useWindowSize();
 
   return (
     <Card.Card className="md-w-[50%] w-full rounded-md px-2 py-5 text-center shadow-lg">
@@ -23,20 +13,18 @@ const LaunchResults: React.FC = () => {
         Resultado de Lançamento
       </Card.CardTitle>
 
-      <Card.CardContent className="flex items-center justify-between px-2">
-        <div className="flex items-center">
-          <FaCheck className="mr-1 text-base text-green-500" />
-          <p className="text-base font-medium uppercase text-gray-500">
-            Sucesso: {successCount}
-          </p>
-        </div>
+      <Card.CardContent className="relative flex flex-col items-center justify-between px-2">
+        {width >= 768 && (
+          <div className="absolute flex w-full flex-row-reverse">
+            <PieChart />
+          </div>
+        )}
 
-        <div className="flex items-center ">
-          <FaTimes className="mr-1 text-base text-red-500" />
-          <p className="text-sm font-medium uppercase text-gray-500">
-            Falha: {failureCount}
-          </p>
-        </div>
+        {width < 768 && (
+          <div className="flex justify-between gap-5">
+            <SuccessAndFailureLaunchesCount />
+          </div>
+        )}
       </Card.CardContent>
     </Card.Card>
   );
---
+++ src/pages/Home/components/SuccessAndFailureLaunchesCount.tsx
@@ -0,0 +1,34 @@
+import useLaunchesStats from "@/hooks/useLaunchStats";
+import React from "react";
+import { FaCheck, FaTimes } from "react-icons/fa";
+
+const SuccessAndFailureLaunchesCount: React.FC = () => {
+  const { isLoading, launchesStats } = useLaunchesStats();
+
+  if (isLoading) return <p>Carregando...</p>;
+
+  const successCount = launchesStats?.reduce(
+    (acc, curr) => acc + curr.successful,
+    0,
+  );
+  const failureCount = launchesStats?.reduce(
+    (acc, curr) => acc + curr.failed,
+    0,
+  );
+
+  return (
+    <>
+      <div className="flex items-center">
+        <FaCheck className="mr-1 text-base text-green-500" />
+        <p className="text-base text-gray-500">Sucesso: {successCount}</p>
+      </div>
+
+      <div className="flex items-center">
+        <FaTimes className="mr-1 text-base text-red-500" />
+        <p className="text-base text-gray-500">Falha: {failureCount}</p>
+      </div>
+    </>
+  );
+};
+
+export default SuccessAndFailureLaunchesCount;
