b675d9dff7fde23056f7f14e4f47e4dedf13385a
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-04 13:47:53 -0300

    refactor: refactor dataAPI to use launchesStats

--- src/pages/Home/components/ChartOfLaunchesByYear.tsx
+++ src/pages/Home/components/ChartOfLaunchesByYear.tsx
@@ -51,106 +51,6 @@ const options = {
   },
 };
 
-const dataAPI = [
-  {
-    rocketName: "Falcon 9 Test Flight",
-    successful: 176,
-    failed: 19,
-    launches: [
-      {
-        launchYear: 2018,
-        launchCount: 20,
-      },
-      {
-        launchYear: 2020,
-        launchCount: 26,
-      },
-      {
-        launchYear: 2013,
-        launchCount: 3,
-      },
-      {
-        launchYear: 2017,
-        launchCount: 18,
-      },
-      {
-        launchYear: 2021,
-        launchCount: 31,
-      },
-      {
-        launchYear: 2015,
-        launchCount: 7,
-      },
-      {
-        launchYear: 2014,
-        launchCount: 6,
-      },
-      {
-        launchYear: 2012,
-        launchCount: 2,
-      },
-      {
-        launchYear: 2010,
-        launchCount: 2,
-      },
-      {
-        launchYear: 2022,
-        launchCount: 60,
-      },
-      {
-        launchYear: 2016,
-        launchCount: 9,
-      },
-      {
-        launchYear: 2019,
-        launchCount: 11,
-      },
-    ],
-  },
-  {
-    rocketName: "Falcon Heavy Test Flight",
-    successful: 3,
-    failed: 2,
-    launches: [
-      {
-        launchYear: 2018,
-        launchCount: 1,
-      },
-      {
-        launchYear: 2022,
-        launchCount: 2,
-      },
-      {
-        launchYear: 2019,
-        launchCount: 2,
-      },
-    ],
-  },
-  {
-    rocketName: "FalconSat",
-    successful: 2,
-    failed: 3,
-    launches: [
-      {
-        launchYear: 2006,
-        launchCount: 1,
-      },
-      {
-        launchYear: 2009,
-        launchCount: 1,
-      },
-      {
-        launchYear: 2008,
-        launchCount: 2,
-      },
-      {
-        launchYear: 2007,
-        launchCount: 1,
-      },
-    ],
-  },
-];
-
 const ChartOfLaunchesByYear: React.FC = () => {
   const { launchesStats, isLoading } = useLaunchesStats();
 
@@ -158,7 +58,7 @@ const ChartOfLaunchesByYear: React.FC = () => {
 
   const yearsSet = new Set<number>();
 
-  dataAPI.forEach((rocket) => {
+  launchesStats?.forEach((rocket) => {
     rocket.launches.forEach((launch) => {
       yearsSet.add(launch.launchYear);
     });
@@ -179,6 +79,7 @@ const ChartOfLaunchesByYear: React.FC = () => {
       backgroundColor: LAUNCHES_GRAPHIC_COLORS[i],
     })),
   };
+
   return (
     <Card.Card className="md-w-[50%] w-full rounded-md px-2 py-5 text-center shadow-lg">
       <Card.CardTitle className="mb-5 text-lg font-semibold uppercase text-gray-600">
