bc6ab8e88d319cd41e62ad18135140ae44856cb7
Matheus Silva <matheus_lsilva@hotmail.com>
2023-12-04 15:01:29 -0300

    refactor: add logo to home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "@/styles/globals.css";
 import Home from "@/pages/Home";
 import Footer from "./components/Footer";
+import Logo from "./pages/Home/components/Logo";
 
 const App: React.FC = () => {
   return (
     <>
+      <Logo />
       <Home />
       <Footer />
     </>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import GraphicsSection from "./components/GraphicsSection";
 import LaunchesWrapper from "./components/LaunchesWrapper";
-import Logo from "./components/Logo";
 
 const Home: React.FC = () => {
   return (
-    <div className="mx-auto flex w-full max-w-[1400px] flex-col items-center ">
-      <Logo />
-      <main className="flex w-full flex-col items-center  px-4">
-        <GraphicsSection />
-        <LaunchesWrapper />
-      </main>
-    </div>
+    <main className="mx-auto mt-24 flex w-full max-w-[1400px] flex-col items-center px-4">
+      <GraphicsSection />
+      <LaunchesWrapper />
+    </main>
   );
 };
 
